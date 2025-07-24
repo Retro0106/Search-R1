6d13851694b97ad9fc50df9ee3555f2f50923d7e
George Zachariah V <george.zachariah.v@oracle.com>
2025-07-24 21:02:26 +0530

    add cpq data

--- search_r1/search/build_index.sh
+++ search_r1/search/build_index.sh
@@ -1,12 +1,12 @@
 
-corpus_file=/your/corpus/jsonl/file # jsonl
-save_dir=/the/path/to/save/index
+corpus_file=data/cpq_search/cpq_knowledge_corpus.jsonl # edit this
+save_dir=data/cpq_search/index # edit this
 retriever_name=e5 # this is for indexing naming
 retriever_model=intfloat/e5-base-v2
 
 # change faiss_type to HNSW32/64/128 for ANN indexing
 # change retriever_name to bm25 for BM25 indexing
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python index_builder.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python search_r1/search/index_builder.py \
     --retrieval_method $retriever_name \
     --model_path $retriever_model \
     --corpus_path $corpus_file \
